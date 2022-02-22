49c22f80a585b353d7d5e8ba4f19ed6b88e15096
p2588 <P2588284@my365.dmu.ac.uk>
2022-02-22 15:21:18 +0000

    Added properties for the table

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int gameID { get; set; }
+        public string GameNm { get; set; }
+        public int Price { get; set; }
+        public int AgeRat { get; set; }
     }
 }
--- Testing3/tstStock.cs
+++ Testing3/tstStock.cs
@@ -7,14 +7,92 @@ namespace Testing3
     [TestClass]
     public class tstStock
     {
-        [TestMethod]
-        public void TestMethod1()
+    [TestMethod]
+        public void InstanceOK()
         {
-
             //create an instance of the class we want to create
-            clsStock Stock = clsStock();
+            clsStock Stock = new clsStock();
             //test to see that it exists
             Assert.IsNotNull(Stock);
         }
+
+        [TestMethod]
+        public void GameIdOK()
+        {
+            //create an instance of the class we want to create
+            clsStock Stock = new clsStock();
+            //create some test data to assign to the property
+            Int32 TestData = 45;
+            //assign the data to the property
+            Stock.gameID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Stock.gameID, TestData);
+        }
+
+        [TestMethod]
+        public void GameNameOK()
+        {
+            //create an instance of the class we want to create
+            clsStock Stock = new clsStock();
+            //create some test data to assign to the property
+            string TestData = "Basketball";
+            //assign the data to the property
+            Stock.GameNm = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Stock.GameNm, TestData);
+        }
+
+        [TestMethod]
+        public void PriceOK()
+        {
+            //create an instance of the class we want to create
+            clsStock Stock = new clsStock();
+            //create some test data to assign to the property
+            Int32 TestData = 45;
+            //assign the data to the property
+            Stock.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Stock.Price, TestData);
+        }
+
+        [TestMethod]
+        public void AvailabilityOK()
+        {
+            //create an instance of the class we want to create
+            clsStock Stock = new clsStock();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            Stock.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Stock.Active, TestData);
+        }
+
+        [TestMethod]
+        public void ReleaseDateOK()
+        {
+            //create an instance of the class we want to create
+            clsStock Stock = new clsStock();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            Stock.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Stock.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void AgeRatingOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AnAddress = new clsStock();
+            //create some test data to assign to the property
+            Int32 TestData = 7;
+            //assign the data to the property
+            AnAddress.AgeRat = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnAddress.AgeRat, TestData);
+        }
+
     }
 }
