37c6b550e7e4bfcbeda1438d395a22c73c36f2e6
44thomash <44thomashunt@gmail.com>
2022-03-06 15:21:50 +0000

    Added Functionality
    Made a Simple Presentation Layer
    Added Functionallity to some Buttons on this

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+   
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStaff
+        clsStaff AnStaff = new clsStaff();
+        //capture the Staff Values
+        AnStaff.Username = txtUsername.Text;
+        AnStaff.Password = txtPassword.Text;
+        AnStaff.Role = txtRole.Text;
+        //AnStaff.StartDate = txtStartDate.Text;
+        AnStaff.Admin = chkAdmin.Checked;
+        //AnStaff.StaffID = txtStaffID.Text;
+        //store the Username in the Session Object
+        Session["AnStaff"] = AnStaff;
+        //naviage to the viewer page
+        Response.Redirect("StaffViewer.aspx");
+
+        
     }
 }
