fa1ef3332bcdbc3d752fdca6c17eca9766007a87
44thomash <44thomashunt@gmail.com>
2022-03-13 17:39:45 +0000

    Added Found Methods and Tests for each bit of Data in the Table

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,12 +4,93 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public bool Admin { get; set; }
-        public DateTime StartDate { get; set; }
-        public int StaffID { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
+        //PRIVATE DATA member for the Staff ID property
+        private Int32 mStaffID;
+        private DateTime mStartDate;
+        private String mUsername;
+        private String mPassword;
+        private String mRole;
+        private Boolean mAdmin;
+        public bool Admin
+        {
+            get
+            {
+                return mAdmin;
+            }
+            set
+            {
+                mAdmin = value;
+            }
+        }
+        public DateTime StartDate
+        {
+            get
+            {
+                return mStartDate;
+            }
+            set
+            {
+                mStartDate = value;
+            }
+        }
+        public int StaffID
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffID = value;
+            }
+        }
+        public string Username 
+        {
+            get 
+            {
+                return mUsername;
+            }
+            set 
+            {
+                mUsername = value;
+            } }
+        public string Password 
+        {
+            get
+            {
+                return mPassword;
+            }
+            set
+            {
+                mPassword = value;
+            }
+        }
+        public string Role
+        {
+            get
+            {
+                return mRole;
+            }
+            set
+            {
+                mRole = value;
+            }
+        }
+
+        public bool Find(int staffID)
+        {
+            //set the private data members to the test data value
+            mStaffID = 21;
+            mStartDate = Convert.ToDateTime("16/9/2015");
+            mUsername = "Username";
+            mPassword = "Password";
+            mRole = "Role";
+            mAdmin = true;
+            //always return true 
+            return true;
+        }
     }
 
 
