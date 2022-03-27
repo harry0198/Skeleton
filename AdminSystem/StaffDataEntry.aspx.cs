d73d832a8c90952fbcf79972ab4d67f2d98768d2
44thomash <44thomashunt@gmail.com>
2022-03-27 16:02:55 +0100

    Changes from Week 23 Work, end of Document

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -32,4 +32,29 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
         
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //creaate an instance of the address Class
+        clsStaff AnStaff = new clsStaff();
+        //variable to store the primary key
+        Int32 StaffID;
+        //variable to store the result of the find operation
+        Boolean Found = false;
+        //get the primary key entered by the user
+        StaffID = Convert.ToInt32(txtStaffID.Text);
+        //find the record
+        Found = AnStaff.Find(StaffID);
+        //if found
+        if (Found == true)
+        {
+            //display the values of the properties in the form
+            txtStaffID.Text = AnStaff.StaffID.ToString();
+            txtUsername.Text = AnStaff.Username;
+            txtPassword.Text = AnStaff.Password;
+            txtRole.Text = AnStaff.Role;
+            txtStartDate.Text = AnStaff.StartDate.ToString();
+            chkAdmin.Checked = AnStaff.Admin;
+        }
+    }
 }
