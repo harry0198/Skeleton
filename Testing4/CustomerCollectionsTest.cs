d77e605e554130700e0ab3fb612544285bd96399
Harry Drummond <P2586351@my365.dmu.ac.uk>
2022-03-29 13:43:25 +0100

    Completed Customer Collections Property class & tests

---
+++ Testing4/CustomerCollectionsTest.cs
@@ -0,0 +1,78 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testing4
+{
+    [TestClass]
+    public class CustomerCollectionsTest
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            Assert.IsNotNull(AllCustomers);
+        }
+
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+
+            TestItem.Active = true;
+            TestItem.ID = 1;
+            TestItem.Password = "Pass";
+            TestItem.Username = "Username";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Address = "LE2 3AB";
+
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+
+            Assert.AreEqual(AllCustomers.CustomerList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisCustomerPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            clsCustomer TestItem = new clsCustomer();
+
+            TestItem.Active = true;
+            TestItem.ID = 1;
+            TestItem.Password = "Pass";
+            TestItem.Username = "Username";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Address = "LE2 3AB";
+
+
+            AllCustomers.ThisCustomer = TestItem;
+
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestItem);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.Active = true;
+            TestItem.ID = 1;
+            TestItem.Password = "Pass";
+            TestItem.Username = "Username";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Address = "LE2 3AB";
+
+            TestList.Add(TestItem);
+
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
+        }
+    }
+}
